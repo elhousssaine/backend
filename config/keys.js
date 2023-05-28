ea1357095a88d8bc0ac2cd4f02e602d23729c416
elhousssaine <124269848+elhousssaine@users.noreply.github.com>
2023-05-28 01:35:43 +0100

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// Add your Mongodb URI here to connect to your database
+const MONGODB_URI = "mongodb+srv://houssin:bYnVatJqK3YSVXXq@egx.kpf6leq.mongodb.net/?retryWrites=true&w=majority"
+
+module.exports = {
+  mongoURI: MONGODB_URI,
+  secretOrKey: "secret",
+};
